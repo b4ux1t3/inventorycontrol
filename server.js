52870b374a87e04c38f63da937f0bc429959d4f5
Chris Pilcher <flippedbauxite@gmail.com>
2016-04-01 18:50:10 -0700

    Getting ejs and express up and running

---
+++ server.js
@@ -0,0 +1,16 @@
+express = require("express");
+
+var app = express();
+var port = 8080;
+var path = require('path')
+
+app.set('view engine', ejs);
+app.set('views', path.resolve(__dirname, 'client', 'views'));
+
+app.get('/' function(req, res)){
+  res.render('index.ejs');
+});
+
+app.listen(port function(){
+  console.log('Server running on port: ' + port);
+})

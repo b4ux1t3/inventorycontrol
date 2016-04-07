9978a6430586cc63fb697808e52e8343c55e49ea
Chris Pilcher <flippedbauxite@gmail.com>
2016-04-07 08:43:05 -0700

    Got navigation working

--- client/js/app.js
+++ client/js/app.js
@@ -1,7 +1,9 @@
 var myApp = angular.module('myApp', [
-  'ngRoute'])
+  'ngRoute',
+  'ui.bootstrap'])
   .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
     $routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: 'homeController'});
+    $routeProvider.when('/about', {templateUrl: 'partials/about.html', controller: 'aboutController'});
     $routeProvider.otherwise({redirectTo: '/home'});
 
     $locationProvider.html5Mode({enabled: true, requireBase: false});
